7bbf504369c57f2b40f74a999cc2271203547118
Ash Walker <ashurstwalker@gmail.com>
2022-05-23 20:29:25 -0400

    beep boop

--- flake.nix
+++ flake.nix
@@ -2,20 +2,26 @@
   description = "static personal website";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    alejandra = {
+      url = github:kamadorueda/alejandra;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = {
     self,
     nixpkgs,
+    alejandra
   }: let
     std = nixpkgs.lib;
   in {
+    formatter = std.mapAttrs (system: pkgs: pkgs.default) alejandra.packages;
     nixosModules.default = {
       config,
       pkgs,
       lib,
       ...
     }: {
-      options.signalwalker.services.ashwalker-net = with lib; {
+      options.services."ashwalker.net" = with lib; {
         enable = mkEnableOption "nginx vhost for ashwalker.net";
         name = mkOption rec {
           type = types.str;
