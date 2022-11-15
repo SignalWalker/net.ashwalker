7eceac4c30559afce1cc2a1db762a2832feb5089
Ash Walker <ashurstwalker@gmail.com>
2022-11-15 11:33:17 -0500

    fix: webfinger

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
             # webfinger (see https://willnorris.com/2014/07/webfinger-with-static-files-nginx/)
             locations."/.well-known/webfinger" = {
               extraConfig = ''
-                if ($request_method != ^(GET|HEAD)$) { return 405; }
+                if ($request_method !~ ^(GET|HEAD)$) { return 405; }
                 set_by_lua $resource 'return ngx.unescape_uri(ngx.req.get_uri_args()["resource"])';
                 if ($resource = "") { return 400; }
                 if ($resource = "acct:ash@ashwalker.net")   { rewrite .* ${wfPath} last; }
