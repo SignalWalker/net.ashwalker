8c136209ae112ea3ce44540c78d50c40ddf1c771
Ash Walker <ashurstwalker@gmail.com>
2022-11-15 11:08:05 -0500

    feat: webfinger

--- flake.nix
+++ flake.nix
@@ -30,22 +30,43 @@
         };
         ssl = {
           enable = mkOption {
-		    type = types.either types.bool (types.enum [ "force" ]);
-			default = false;
-		  };
+            type = types.either types.bool (types.enum ["force"]);
+            default = false;
+          };
         };
       };
       config = let
         cfg = config.services.ashwalker-net;
       in
         lib.mkIf cfg.enable {
-          services.nginx.virtualHosts = {
-            "${cfg.name}" = {
-              enableACME = cfg.ssl.enable != false;
-              addSSL = cfg.ssl.enable == true;
-              forceSSL = cfg.ssl.enable == "force";
-              root = ./src;
-              locations."/".root = ./src;
+          services.nginx.virtualHosts."${cfg.name}" = let
+            wfPath = "/well-known/webfinger.json";
+          in {
+            enableACME = cfg.ssl.enable != false;
+            addSSL = cfg.ssl.enable == true;
+            forceSSL = cfg.ssl.enable == "force";
+            root = ./src;
+            serverAliases = ["www.ashwalker.net"];
+            # webfinger (see https://willnorris.com/2014/07/webfinger-with-static-files-nginx/)
+            locations."/.well-known/webfinger" = {
+              extraConfig = ''
+                if ($request_method != ^(GET|HEAD)$) { return 405; }
+                set_by_lua $resource 'return ngx.unescape_uri(ngx.req.get_uri_args()["resource"])';
+                if ($resource = "") { return 400; }
+                if ($resource = "acct:ash@ashwalker.net")   { rewrite .* ${wfPath} last; }
+                if ($resource = "mailto:ash@ashwalker.net") { rewrite .* ${wfPath} last; }
+                if ($resource = "https://ashwalker.net")    { rewrite .* ${wfPath} last; }
+                if ($resource = "https://ashwalker.net/")   { rewrite .* ${wfPath} last; }
+              '';
+            };
+            locations."${wfPath}" = {
+              extraConfig = ''
+                types { application/jrd+json json; }
+                add_header Access-Control-Allow-Origin "*";
+              '';
+            };
+            locations."/.well-known" = {
+              root = ./src/well-known;
             };
           };
         };
