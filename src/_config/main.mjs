abdaf3d25f372db4ae02908be825c87f2bc24401
Ash Walker <ashurstwalker@gmail.com>
2024-10-06 22:29:17 -0400

    fix: migrate most things from njk to webc

--- src/_config/main.mjs
+++ src/_config/main.mjs
@@ -1,3 +1,8 @@
 export default function(eleventyConfig) {
-
+	eleventyConfig.addJavaScriptFunction("tagsToEntryClass", function(tags) {
+		return ['h-entry'].concat(tags.filter((tag) => tag == 'article' || tag == 'fiction')).join(' ');
+	});
+	eleventyConfig.addJavaScriptFunction("isPublicTag", function(tag) {
+		return tag != "post";
+	});
 }
